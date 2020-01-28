e83cf11ec6ca245dc69d98eded8137da1877f6c5
Isaac Atia-Abugbilla <29587987+atiaisaac@users.noreply.github.com>
2020-01-28 20:11:59 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -101,8 +101,6 @@ systemctl start sample.service
 
 # configuring network address translation 
 # replace ppp0 with interface of source of internet
-# configuring network address translation 
-# replace ppp0 with eth0 if using ethernet instead of dialup modem
 # also replace wlan0 with the correct interface name
 iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE  
 iptables -A FORWARD -i ppp0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
